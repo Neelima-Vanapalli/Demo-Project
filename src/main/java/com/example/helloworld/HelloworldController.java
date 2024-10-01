d8acd52d97e6de23310d4c6bbac934c2054a6816
Neelima-Vanapalli <vanapallineelima07@gmail.com>
2024-10-01 23:41:40 +0530

    hello world

---
+++ src/main/java/com/example/helloworld/HelloworldController.java
@@ -0,0 +1,11 @@
+package com.example.helloworld;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class HelloworldController {
+	@GetMapping("/welcome")
+	public String getName() {
+		return "Neelima";
+	}
+}
